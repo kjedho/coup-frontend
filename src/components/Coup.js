0fdaa08c39d398c18bdf35c610241532414b923c
Kjell Dhoore <kjelldhoore@gmail.com>
2024-07-07 22:50:46 +0200

    create first draft of landing page

--- src/components/Coup.js
+++ src/components/Coup.js
@@ -1,5 +1,18 @@
 import React from "react";
-import "../components/Coup.css";
+import { Typography, Container, Stack, TextField, Button } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import './Coup.css';
+
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#0073e6",
+      },
+      secondary: {
+        main: "#3399ff",
+      },
+    },
+  });
 
 class Coup extends React.Component {
     initialize = () => {
@@ -14,9 +27,39 @@ class Coup extends React.Component {
 
     render() {
         return (
-            <div>
-            <h1>Coup</h1>
-            </div>
+            <ThemeProvider theme={theme}>
+            <Container maxWidth="md" sx={{marginTop: "10vh"}}>
+                <Typography variant="h2" textAlign="center">
+                    Welcome to Coup online!
+                </Typography>
+                <Typography variant="h4" textAlign="center" color="primary">
+                    <em>The place to be to play Coup privately with friends.</em>
+                </Typography>
+                <Stack direction="row" alignItems="center" justifyContent="space-evenly" marginTop="10vh">
+                    <img
+                        src="https://dragonsdengames.com/wp-content/uploads/2020/10/Coup.jpg"
+                        alt="Coup"
+                        height="400vh"
+                    />
+                    <Stack direction="column" alignItems="center" >
+                        <TextField 
+                            label="Enter your name"
+                            variant="filled"
+                            sx={{
+                                width: "15vw",
+                                marginBottom: "1vh",
+                                '& .MuiInputBase-input': {
+                                    backgroundColor: "#ffffff",
+                                }
+                            }}
+                        />
+                        <Button variant="contained" backgroundColor="secondary" sx={{ width: "15vw", marginTop: "1vh" }}>
+                            Create private lobby
+                        </Button>
+                    </Stack>
+                </Stack>
+            </Container>
+            </ThemeProvider>
         );
     }
 }
