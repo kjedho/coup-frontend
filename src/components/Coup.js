e622b7c6e1771baa4daf14c4ac905589f9469fef
Kjell Dhoore <kjelldhoore@gmail.com>
2024-07-07 23:26:42 +0200

    add number of players dropdown

--- src/components/Coup.js
+++ src/components/Coup.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Typography, Container, Stack, TextField, Button } from '@mui/material';
+import { Typography, Container, Stack, TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { SelectChangeEvent } from '@mui/material/Select';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './Coup.css';
 
@@ -15,14 +16,21 @@ const theme = createTheme({
   });
 
 class Coup extends React.Component {
+
     initialize = () => {
-        return {};
+        return { players: 0 };
     };
 
     state = this.initialize();
 
     reset = () => {
         this.setState(this.initialize());
+    };
+
+    handleChange = (event: SelectChangeEvent) => {
+        this.setState({
+            players: event.target.value
+        });
     };
 
     render() {
@@ -42,18 +50,43 @@ class Coup extends React.Component {
                         height="400vh"
                     />
                     <Stack direction="column" alignItems="center" >
-                        <TextField 
-                            label="Enter your name"
-                            variant="filled"
-                            sx={{
-                                width: "15vw",
-                                marginBottom: "1vh",
-                                '& .MuiInputBase-input': {
-                                    backgroundColor: "#ffffff",
-                                }
-                            }}
-                        />
-                        <Button variant="contained" backgroundColor="secondary" sx={{ width: "15vw", marginTop: "1vh" }}>
+                        <Stack direction="row" alignItems="center" >
+                            <TextField 
+                                label="Enter your name"
+                                variant="filled"
+                                sx={{
+                                    width: "11.5vw",
+                                    marginRight: "0.25vw",
+                                    '& .MuiInputBase-input': {
+                                        backgroundColor: "#ffffff",
+                                    }
+                                }}
+                            />
+                            <FormControl variant="filled">
+                                <InputLabel id="number-of-players-label">Players</InputLabel>
+                                <Select
+                                    labelId="number-of-players-label"
+                                    id="number-of-players"
+                                    label="Players"
+                                    onChange={this.handleChange}
+                                    sx={{
+                                        width: "8vw",
+                                        marginLeft: "0.25vw",
+                                        '& .MuiInputBase-input': {
+                                            backgroundColor: "#ffffff",
+                                        },
+                                        backgroundColor: "#ffffff"                                        
+                                    }}
+                                >
+                                    <MenuItem value={2}>Two</MenuItem>
+                                    <MenuItem value={3}>Three</MenuItem>
+                                    <MenuItem value={4}>Four</MenuItem>
+                                    <MenuItem value={5}>Five</MenuItem>
+                                    <MenuItem value={6}>Six</MenuItem>
+                                </Select>
+                            </FormControl>
+                        </Stack>
+                        <Button variant="contained" backgroundColor="secondary" sx={{ width: "20vw", marginTop: "0.5vw" }}>
                             Create private lobby
                         </Button>
                     </Stack>
