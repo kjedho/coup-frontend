14b73df29b70a1f9980cba44cadbf7bcfbfa2852
Kjell Dhoore <kjelldhoore@gmail.com>
2024-07-15 23:46:52 +0200

    add join private lobby option

--- src/components/Creation.jsx
+++ src/components/Creation.jsx
@@ -1,9 +1,10 @@
 
 import { useState } from 'react';
-import { Stack, TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
+import { Box, Stack, TextField, Button, FormControl, InputLabel, Select, MenuItem, Typography } from '@mui/material';
 
 function Creation() {
     const [numberOfPlayers, setNumberOfPlayers] = useState(2);
+    const columnWidth = "15vw";
 
     return (
         <Stack direction="row" alignItems="center" justifyContent="space-evenly">
@@ -12,39 +13,55 @@ function Creation() {
                 alt="Coup"
                 height="400vh"
             />
-            <Stack direction="column" alignItems="center" >
-                <Stack direction="row" alignItems="center" >
+            <Stack direction="column" alignItems="center" justifyContent="space-evenly" height="45vh" >
+                <TextField 
+                    label="Enter your name"
+                    variant="outlined"
+                    sx={{
+                        width: columnWidth,
+                        marginBottom: "4vh" 
+                    }}
+                />
+                <Box>
+                    <Stack direction="row" alignItems="center" >
+                        <FormControl variant="filled">
+                            <InputLabel id="number-of-players-label">Pick number of players</InputLabel>
+                            <Select
+                                labelId="number-of-players-label"
+                                id="number-of-players"
+                                label="Players"
+                                onChange={(event) => setNumberOfPlayers(event.target.value)}
+                                sx={{
+                                    width: columnWidth
+                                }}
+                            >
+                                <MenuItem value={2}>Two</MenuItem>
+                                <MenuItem value={3}>Three</MenuItem>
+                                <MenuItem value={4}>Four</MenuItem>
+                                <MenuItem value={5}>Five</MenuItem>
+                                <MenuItem value={6}>Six</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Stack>
+                    <Button variant="contained" sx={{ width: columnWidth, marginTop: "1vh" }}>
+                        Create private lobby
+                    </Button>
+                </Box>
+                <Typography variant="h6" textAlign="center" color="primary">
+                    OR
+                </Typography>
+                <Stack direction="column" alignItems="center" >
                     <TextField 
-                        label="Enter your name"
+                        label="Enter your lobby link"
                         variant="filled"
                         sx={{
-                            width: "11.5vw",
-                            marginRight: "0.25vw",
+                            width:columnWidth,
                         }}
                     />
-                    <FormControl variant="filled">
-                        <InputLabel id="number-of-players-label">Players</InputLabel>
-                        <Select
-                            labelId="number-of-players-label"
-                            id="number-of-players"
-                            label="Players"
-                            onChange={(event) => setNumberOfPlayers(event.target.value)}
-                            sx={{
-                                width: "8vw",
-                                marginLeft: "0.25vw",
-                            }}
-                        >
-                            <MenuItem value={2}>Two</MenuItem>
-                            <MenuItem value={3}>Three</MenuItem>
-                            <MenuItem value={4}>Four</MenuItem>
-                            <MenuItem value={5}>Five</MenuItem>
-                            <MenuItem value={6}>Six</MenuItem>
-                        </Select>
-                    </FormControl>
+                    <Button variant="contained" sx={{ width: columnWidth, marginTop: "1vh" }}>
+                        Join private lobby
+                    </Button>
                 </Stack>
-                <Button variant="contained" sx={{ width: "20vw", marginTop: "0.5vw" }}>
-                    Create private lobby
-                </Button>
             </Stack>
         </Stack>
     );
